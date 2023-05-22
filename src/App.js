cefe784e6e0f327291554bed1416ba44bdba25e5
daviddam1 <ddamja00@fesb.hr>
2023-05-22 22:33:36 +0200

    weather-App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api ={
+  key: "0155ce25f5734419bffce08eb11f2eb9",
+  base: "https://api.openweathermap.org/data/2.5/"  
 }
 
-export default App;
+export default function App(){
+  
+  const [inputValue,setInputValue] = React.useState('')
+  const [weather,setWeather] = React.useState({})
+
+  const search = evt =>{
+    if(evt.key === "Enter"){
+      fetch(`${api.base}weather?q=${inputValue}&units=metric&APPID=${api.key}`)
+      .then(res =>res.json())
+      .then(result =>{
+        setWeather(result)
+        setInputValue('')
+        console.log(weather)
+      })
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  return(
+  <div className={(typeof weather.main!='undefined')?
+  ((weather.main.temp>16)?'app-warm':'app'):'app'
+  } >
+    <main>
+      <div className="search-bar">
+        <input
+          value={inputValue}
+          onChange={(e) =>setInputValue(e.target.value)} 
+          placeholder="Search"
+          onKeyPress={search}>          
+        </input>
+      </div>
+      {(typeof weather.main!= "undefined")?
+      (<div className="new-app">
+        <div className="location">
+        {weather.name}, {weather.sys.country}
+        </div>
+        <div className="date">
+        {dateBuilder(new Date())}
+        </div>
+        <div className="temperature">
+          {Math.round(weather.main.temp)}°C
+        </div>
+        <div className="weather">
+        {weather.weather[0].main}
+        </div>
+        </div>):('')}      
+    </main>
+  </div>
+
+  )
+}
